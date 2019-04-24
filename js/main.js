2f851bc48265a95e60e76fcfbda000542a9ce66f
Mudit Rajpal <muditrajpal@197NODMB22044.local>
2019-04-24 18:31:47 +0530

    updated file path

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
 "use strict";
 
 // Make sure sw are supported
-var parser = new UAParser();
-var result = parser.getResult();
-console.log('%c Browser Type ', 'background: blue; color: #bada55', result.browser);
-console.log('%c Mobile Type ', 'background: red; color: #bada55', result.device);
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('../sw_cached_pages.js').then(function (reg) {
-      console.log('sw reg')
-      return console.log('Service Worker: Registered (Pages)');
-    }).catch(function (err) {
-      return console.log("Service Worker: Error: ".concat(err));
-    });
-  });
-} else {
-  console.log('%c Does Not support ServiceWorker! ', 'background: #222; color: #bada55', navigator);
-}
